34630a04dc2b5868db4f99ef16f89383a3e7b2cb
Kevin krzysztof Zak <kzak@e4r6p1.42roma.it>
2023-05-23 14:32:25 +0200

    ex00

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: kzak <kzak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 09:26:59 by kzak              #+#    #+#             */
-/*   Updated: 2023/05/22 16:25:57 by kzak             ###   ########.fr       */
+/*   Updated: 2023/05/23 14:30:29 by kzak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 Bitcoin::Bitcoin() {
 	std::ifstream data("data.csv");
-	std::string line;
-	size_t len;
-	float number;
+	std::string line = "";
 
 	while (std::getline(data, line)) {
 		if (line != "date,exchange_rate") {
-			len = line.find(',');
-			std::istringstream(line.substr(len + 1, line.length())) >> number;
-			_data[line.substr(0, len)] = number;
+			float number = 0.0;
+			size_t commaIndex = line.find(',');
+			std::string numberStr = line.substr(commaIndex + 1, line.size());
+			std::istringstream iss(numberStr);
+			iss >> number;
+			_data[line.substr(0, commaIndex)] = number;
 		}
 	}
 	data.close();
@@ -41,18 +42,62 @@ Bitcoin& Bitcoin::operator=(const Bitcoin& other) {
 
 Bitcoin::~Bitcoin() {}
 
+int is_number(const std::string& s) {
+	int i = 0;
+	if (s[i] == ' ' || s[i] == '-')
+		i++;
+	if (s[i] == '-' && s[i - 1] != '-')
+		i++;
+	while (s[i]) {
+		if (!isdigit(s[i]) && s[i] != '.')
+			return 1;
+		i++;
+	}
+	return 0;
+}
+
+bool check_line(std::string line) {
+	if (line.size() < 12) {
+		std::cout << "Error: bad input => " << line << std::endl;
+		return false;
+	}
+	float number = 0.0;
+	size_t pipeIndex = line.find("|");
+	if (pipeIndex >= line.size()) {
+		std::cout << "Error: no pipe => " << line << std::endl;
+		return false;
+	}
+
+	std::string numberStr = line.substr(pipeIndex + 1, line.size());
+	if (!is_number(numberStr)) {
+		std::istringstream iss(numberStr);
+		iss >> number;
+	} else {
+		std::cout << "Error: no valid value => " << line << std::endl;
+		return false;
+	}
+	
+	return true;
+}
+
+void check_convert(std::string line) {
+	if (line != "date | value") {
+		if (check_line(line))
+			return;
+		
+	}
+}
+
 int Bitcoin::bitExchange(char* argv) {
 	std::ifstream input(argv);
 	if (!input.good()) {
-		std::cerr << "Error: could not open file." << std::endl;
+		std::cerr << "\033[0;31mError: could not open file.\033[0m" << std::endl;
 		return 1;
 	}
 
 	std::string line;
 	while (std::getline(input, line)) {
-		if (line != "date | value") {
-
-		}
+		check_convert(line);
 	}
 	input.close();
 	return 0;
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kzak <kzak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 09:26:56 by kzak              #+#    #+#             */
-/*   Updated: 2023/05/22 16:14:28 by kzak             ###   ########.fr       */
+/*   Updated: 2023/05/23 11:09:01 by kzak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(int argc, char** argv) {
 	if (argc != 2) {
-		std::cerr << "Error: could not open file." << std::endl;
+		std::cerr << "\033[0;31mError: could not open file.\033[0m" << std::endl;
 		return 1;
 	}
 	Bitcoin bit;

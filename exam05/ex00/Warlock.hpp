21cb4b1b68047bcc426a6f81f1f8bb7b00e1eb3e
Kevin krzysztof Zak <kzak@e4r6p1.42roma.it>
2023-05-30 16:36:38 +0200

    exam

--- exam05/ex00/Warlock.hpp
+++ exam05/ex00/Warlock.hpp
@@ -1,25 +1,30 @@
+
 #ifndef WARLOCK_HPP
 # define WARLOCK_HPP
 
 # include <iostream>
 # include <string>
 
+using namespace std;
+
 class Warlock {
 	private:
-		std::string _name;
-		std::string _title;
-
+		string name;
+		string title;
 	public:
-		Warlock(const std::string& name, const std::string& title);
+		Warlock(const string& name, const string& title) : name(name), title(title) {
+			cout << name << ": This look .." << endl;
+		}
+		~Warlock() {cout << name << ": My job .." << endl;}
 		Warlock(const Warlock&) = delete;
 		Warlock& operator=(const Warlock&) = delete;
-		~Warlock();
 
-		void setTitle(const std::string& title);
-		const std::string& getName() const;
-		const std::string& getTitle() const;
+		const string& getName() const {return name;}
+		const string& getTitle() const {return title;}
 
-		void introduce() const;
+		void setTitle(const string& title) {this->title = title;}
+
+		void introduce() const { cout << name << "I am" << name << ", " << title << "!" << endl;}
 };
 
 #endif
